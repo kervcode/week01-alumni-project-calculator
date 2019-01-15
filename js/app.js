6e3c7aaeaf07d97a6c07e07398eb2dea07c60dcc
Kervintz Noel <Kervintz84@hotmail.com>
2019-01-14 21:49:35 -0500

    upload local directory

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let one = document.getElementById('one'),
+    two = document.getElementById('two'),
+    three = document.getElementById('three'),
+    four = document.getElementById('four'),
+    five = document.getElementById('five'),
+    six = document.getElementById('six'),
+    seven = document.getElementById('seven'),
+    height = document.getElementById('height'),
+    nine = document.getElementById('nine'),
+    division = document.getElementById('division'),
+    multiplication = document.getElementById('multiplication'),
+    addition = document.getElementById('addition'),
+    substraction = document.getElementById('substraction'),
+    equal = document.getElementById('equal'),
+    output = document.getElementById('output');
+
+//initiate calculator object
+
+
+
+
+    
